687b06aa5502be34a015f07009436a8065961b68
nil1729 <nilanjan172nsvian@gmail.com>
2021-07-28 23:11:03 +0530

    ENV Vars

--- config/db.js
+++ config/db.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
-const config = require('config');
 
 module.exports = async () => {
-    try {
-        await mongoose.connect(config.get('mongoURI'), {
-            useCreateIndex: true,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log('MongoDB Connected....');
-    } catch (e) {
-        console.log(e);
-        console.log('Refused to Connect');
-    }
+	try {
+		await mongoose.connect(process.env.MONGO_URI, {
+			useCreateIndex: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+		console.log('MongoDB Connected....');
+	} catch (e) {
+		console.log(e);
+		console.log('Refused to Connect');
+	}
 };
--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const URL = require('./models/URL');
@@ -6,83 +10,84 @@ const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path');
 
-// Database Connect 
+// Database Connect
 connectDB();
 
 // Body Parser Setup
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({extended: true}));
+app.use(express.json({ extended: true }));
 app.set('view engine', 'ejs');
 
 // Security
 app.use(cors());
 
 // Middleware Setup
-app.use((req, res, next)=> {
-    req.baseUrl = `${req.protocol}://${req.get('host')}`;
-    next();  
+app.use((req, res, next) => {
+	req.baseUrl = `${req.protocol}://${req.get('host')}`;
+	next();
 });
 
 // Create Short URL
-app.post('/api/v2/shorten', async(req, res) => {
-     try {
-        const {longURL} = req.body;
-        console.log(longURL);
-        let url = await URL.findOne({longURL});
-        if (url) {
-            return  res.status(200).json({
-                ...url._doc,
-                shortURL: `${req.baseUrl}/${url.shortID}`,
-            });
-        }
-        url = await URL.create({ 
-            longURL, 
-            shortID: shortid.generate()
-        });
-        res.status(200).json({
-            ...url._doc,
-            shortURL: `${req.baseUrl}/${url.shortID}`,
-        });
-     } catch (e) {
-        console.log(e);
-        return res.status(500).json({
-              msg: 'Server Error'
-        });
-     }
+app.post('/api/v2/shorten', async (req, res) => {
+	try {
+		const { longURL } = req.body;
+		console.log(longURL);
+		let url = await URL.findOne({ longURL });
+		if (url) {
+			return res.status(200).json({
+				...url._doc,
+				shortURL: `${req.baseUrl}/${url.shortID}`,
+			});
+		}
+		url = await URL.create({
+			longURL,
+			shortID: shortid.generate(),
+		});
+		res.status(200).json({
+			...url._doc,
+			shortURL: `${req.baseUrl}/${url.shortID}`,
+		});
+	} catch (e) {
+		console.log(e);
+		return res.status(500).json({
+			msg: 'Server Error',
+		});
+	}
 });
-
 
 // Clicks on ShortURLs
-app.get('/:shortID', async(req, res) => {
-    try {
-        const {shortID} = req.params;
-        let url = await URL.findOne({shortID});
-        if(!url){
-            return res.status(404).json({
-                msg: 'No valid URLs found.'
-            });
-        }
-        url.clicks++;
-        await url.save();
-        res.redirect(url.longURL);
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            msg:'Server Error'
-        });   
-    }
+app.get('/:shortID', async (req, res) => {
+	try {
+		const { shortID } = req.params;
+		let url = await URL.findOne({ shortID });
+		if (!url) {
+			return res.status(404).json({
+				msg: 'No valid URLs found.',
+			});
+		}
+		url.clicks++;
+		await url.save();
+		res.redirect(url.longURL);
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({
+			msg: 'Server Error',
+		});
+	}
 });
 
-// Serve static  assets in Production 
-if(process.env.NODE_ENV === 'production'){
-    // set Static Folder
-    app.use(express.static('client/build'));
+// Serve static  assets in Production
+if (process.env.NODE_ENV === 'production') {
+	// set Static Folder
+	app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-};
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 // Server PORT Setup
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, process.env.IP, () => {
-    console.log(`Server started on port ${PORT}`);
+	console.log(`Server started on port ${PORT}`);
 });
